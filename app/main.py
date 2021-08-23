828eb91d91454286b9f27591ff7dd40115cc8afd
pl465j <pl465j@att.com>
2021-08-23 19:05:50 +0530

    Added PV and PVC

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from flask import Flask
+import os
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    day = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     with open('/log/logfile.log','a') as f:
-      f.write('Hello Log \n')
+      f.write(day+' : Hello Log '+os.getenv('ENV','dev')+ ' \n')
       f.close()
-    return "Hello from Python!"
+    return "Hello from Python! - "+day
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=6050)
